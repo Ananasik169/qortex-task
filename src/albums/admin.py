62c64060f8682e73fe20d41447974dbaff29d0d6
Yura <borzikovu32@gmail.com>
2023-03-13 13:48:37 +0300

    admin site

--- src/albums/admin.py
+++ src/albums/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from . models import Singer, Song, SongNumber, Album
 
-# Register your models here.
+
+@admin.register(Singer)
+class SingerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    fields = ['name']
+    list_filter = ('name',)
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    fields = ['name']
+    list_filter = ('name',)
+
+
+@admin.register(SongNumber)
+class SongNumberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'album', 'song', 'number_song',)
+    fields = ['album', 'song', 'number_song']
+    list_filter = ('album', 'song',)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'year', 'singer',)
+    fields = ['name', 'year', 'singer']
+    list_filter = ('year', 'singer')
---
+++ src/albums/tests/test_models.py
@@ -0,0 +1,8 @@
+# from typing import List, Optional
+
+from albums.models import Singer
+import pytest
+
+
+@pytest.fixture
+def singer()
